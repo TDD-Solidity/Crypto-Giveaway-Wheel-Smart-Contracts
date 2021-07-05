f3760ad4f50e2443435fb0ed1ba377253529ea27
JimLynchCodes <mrdotjim@gmail.com>
2021-07-05 14:58:39 -0400

    building out smart contract functions

--- contracts/CryptoGiveawayWheelEthOnEth.sol
+++ contracts/CryptoGiveawayWheelEthOnEth.sol
@@ -9,29 +9,80 @@ pragma solidity >=0.4.22 <0.9.0;
 contract CryptoGiveawayWheelEthOnEth {
     address public owner = msg.sender;
 
+    address sponsorAddress;
+    string sponsorName;
+    string sponsorImage;
+    string sponsorLink;
+
+    bool registrationPhaseOpen = false;
+    bool claimWinningsPhaseOpen = false;
+
+    address[] participantAddresses;
+
+    address[] addressesRegisteredForEvent;
+
+    mapping(address => string) participantNames;
+
+    mapping(address => bool) participantRegistered;
+
+    mapping(address => uint256) participantWinnings;
+
     constructor() public {}
 
-   /**
-    *  Functions for Admins
-    */
+    /**
+     *  Functions for Admins
+     */
 
     modifier adminOnly() {
-        require(
-            msg.sender == owner,
-            "This function is restricted to the contract's owner"
-        );
+        require(msg.sender == owner, "This function is restricted to admin");
         _;
     }
 
-    function startEvent() public adminOnly {}
+    function startEvent() public adminOnly {
+        registrationPhaseOpen = true;
 
-    function spinWheel() public adminOnly {}
+        // warning: unbounded for loop is kind of an anti-pattern...
 
-    function endEvent() public adminOnly {}
+        for (uint256 i = 0; i < addressesRegisteredForEvent.length; i++) {
+            delete participantWinnings[participantAddresses[i]];
+            delete participantRegistered[participantAddresses[i]];
+        }
 
-    function updateSponsorAddress() public adminOnly {}
+        delete addressesRegisteredForEvent;
 
-    function updateRegisteredRecipient() public adminOnly {}
+        delete sponsorAddress;
+        delete sponsorName;
+        delete sponsorImage;
+        delete sponsorLink;
+    }
+
+    function spinWheel() public adminOnly {
+        registrationPhaseOpen = false;
+
+        // TODO - use chainlink (or something similar) to randomly select a winner.
+
+        // TODO - use OpenZeppelin's "PaymentSplitter" to split eth...
+
+        claimWinningsPhaseOpen = true;
+    }
+
+    function endEvent() public adminOnly {
+        claimWinningsPhaseOpen = false;
+
+        // TODO - send remaining crypto / NFTs in contract to dev wallet.
+    }
+
+    function updateSponsorAddress(address newSponsorAddress) public adminOnly {
+        sponsorAddress = newSponsorAddress;
+    }
+
+    function updateRegisteredRecipient(
+        address participantAddress,
+        string memory participantName
+    ) public adminOnly {
+        participantNames[participantAddress] = participantName;
+        participantAddresses.push(participantAddress);
+    }
 
     /**
      *  Functions for Contributor(s)
@@ -39,27 +90,35 @@ contract CryptoGiveawayWheelEthOnEth {
 
     modifier contributorOnly() {
         require(
-            msg.sender == owner,
-            "This function is restricted to the contract's owner"
+            msg.sender == sponsorAddress,
+            "This function is restricted to contributors"
         );
         _;
     }
 
     modifier onlyContributorOrAdmin() {
         require(
-            msg.sender == owner,
-            "This function is restricted to the contract's owner"
+            msg.sender == owner || msg.sender == sponsorAddress,
+            "This function is restricted to contributors or admin"
         );
         _;
     }
 
-    function contributeToPot() public contributorOnly {}
+    function contributeToPot() public contributorOnly {
+        // TODO send eth to dev wallet
+    }
 
-    function updateSponsorName() public onlyContributorOrAdmin {}
+    function updateSponsorName(string memory newName) public onlyContributorOrAdmin {
+        sponsorName = newName;
+    }
 
-    function updateSponsorImage() public onlyContributorOrAdmin {}
+    function updateSponsorImage(string memory newImage) public onlyContributorOrAdmin {
+        sponsorImage = newImage;
+    }
 
-    function updateSponsorLink() public onlyContributorOrAdmin {}
+    function updateSponsorLink(string memory newLink) public onlyContributorOrAdmin {
+        sponsorLink = newLink;
+    }
 
     /**
      *  Functions for Recipients
@@ -67,14 +126,50 @@ contract CryptoGiveawayWheelEthOnEth {
 
     modifier onlyRecipient() {
         require(
-            msg.sender == owner,
-            "This function is restricted to the contract's owner"
+            msg.sender != owner && msg.sender != sponsorAddress,
+            "This function is restricted to recipients"
         );
         _;
     }
 
-    function registerForEvent() public onlyRecipient {}
+    modifier isRegistered() {
+        require(
+            participantRegistered[msg.sender] === true,
+            "This function is restricted to recipients"
+        );
+        _;
+    }
 
-    function claimWinnings() public onlyRecipient {}
+    modifier isRecognizedRecipient() {
+        require(
+            participantNames[msg.sender],
+            "This function is restricted to recognized participants"
+        );
+        _;
+    }
+
+    modifier registrationIsOpen() {
+        require(
+            registrationPhaseOpen === true,
+            "This function can only be called when registration is open"
+        );
+        _;
+    }
+
+    modifier claimWinningsIsOpen() {
+        require(
+            registrationPhaseOpen === true,
+            "This function can only be called when winnings claiming phase is open"
+        );
+        _;
+    }
+
+    function registerForEvent() public onlyRecipient registrationIsOpen isRegistered isRecognizedRecipient {
+
+    }
+
+    function claimWinnings() public onlyRecipient claimWinningsIsOpen {
+
+    }
 
 }
